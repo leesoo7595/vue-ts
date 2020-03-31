cef5ba5659ba8cd8b3f0d75d91911f61a95d9be6
천이수 <lstsun@cheon-isuui-MacBookPro-2.local>
2020-03-31 14:42:05 +0900

    vue-ts config & component decorator

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+        <img src="../assets/logo.png" alt="Vue logo">
+        <Message></Message>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+    import Message from '@components/Message.vue';
+
+    @Component({
+        components: {
+            Message,
+        },
+    })
+    export default class Home extends Vue {}
+</script>
