ca3bb254fbe9454b5e8c54dfe4bc1d28a9df7d37
leesoo7595 <isoo7510@gmail.com>
2020-04-01 18:08:44 +0900

    decorator provide/inject

--- src/components/Children.vue
+++ src/components/Children.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
-        <button @click="counter">자식에서 숫자를 증가시킨다.</button>
+        {{message}}
     </div>
 </template>
 
 <script lang="ts">
-    import {Component, Prop, Vue, Watch, Emit} from 'vue-property-decorator';
+    import {Vue, Component, Inject} from 'vue-property-decorator';
 
     @Component
     export default class Children extends Vue {
         
-        @Emit()
-        counter() {
-             console.log('counter');
-        }
+        // 프로퍼티 데코레이터는 초기화를 안하면 오류를 뱉는다.
+        // 1. optional(?)하게 주면 해당 오류를 넘어갈 수 있다.
+        // 2. essential(!)하게 주면 의도되었다고 인식하여 오류를 넘어갈 수 있다.
+        @Inject() readonly message!: string;
     }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,12 @@
 <template>
     <div class="home">
         <img src="../assets/logo.png" alt="Vue logo">
-        <Children @counter="counter"></Children>
-        <p>
-            부모에서 숫자를 보여준다. : {{count}}
-        </p>
+        <Children></Children>
     </div>
 </template>
 
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
+    import { Vue, Component, Provide } from 'vue-property-decorator';
     import Children from '@/components/Children.vue';
 
     @Component({
@@ -18,10 +15,8 @@
         },
     })
     export default class Home extends Vue {
-        count: number = 0;
-
-        counter() {
-            this.count++;
-        }
+        
+        @Provide('message') msg: string = 'provide/inject example';
+        
     }
 </script>
