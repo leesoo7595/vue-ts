4b4a1349e3a8ec7ab0ec9a34769866b0fcefe222
leesoo7595 <isoo7510@gmail.com>
2020-04-03 17:14:26 +0900

    vuex module

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
   <div id="app">
-    <Counter />
-    <button @click="increase">+</button>
-    <button @click="decrease">-</button>
+    {{$store.state}}
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import Counter from '@/components/Counter.vue';
 
 @Component({
   components: {
-    Counter
   },
 })
 export default class App extends Vue {
-  increase() {
-    this.$store.dispatch("increase");
-  }
-
-  decrease() {
-    this.$store.dispatch("decrease");
+  created() {
+    console.log(this.$store);
+    this.$store.dispatch('moduleA/setRootData', 'test');
   }
 }
 </script>
