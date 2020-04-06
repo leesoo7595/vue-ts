0b936edb630ab8e96603e082fc5581b5e34d0034
leesoo7595 <isoo7510@gmail.com>
2020-04-06 13:17:31 +0900

    module vuex-class

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
   <div id="app">
-    {{$store.state}}
+     <Counter />
+     <button @click="increase">+</button>
+     <button @click="decrease">-</button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import {Action, Mutation} from 'vuex-class';
+import Counter from '@/components/Counter.vue';
 
 @Component({
   components: {
+    Counter,
   },
 })
 export default class App extends Vue {
-  created() {
-    console.log(this.$store);
-    this.$store.dispatch('moduleA/setRootData', 'test');
-  }
+  
+  @Action readonly increase;
+
+  @Action readonly decrease;
+
+  @Mutation readonly setCount;
 }
 </script>
 
---
+++ src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{$store.getters.count}}
+    </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+import {Getter, State} from 'vuex-class';
+
+@Component
+export default class Counter extends Vue {
+
+  @State readonly count!: number;
+}
+</script> 
